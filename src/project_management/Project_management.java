fe267dab7448e58ccc5673bd177844beab01344d
“sanketRmeshram” <meshramsanket2000@gmail.com>
2019-04-03 21:16:21 +0530

    uploading file

---
+++ src/project_management/Project_management.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project_management;
+
+/**
+ *
+ * @author sanket
+ */
+public class Project_management {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Sign_up().setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
